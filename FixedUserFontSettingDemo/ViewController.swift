347ddf7053ca60d5eeb5597889236bfd9c0a7f6d
inexcii <ralph.zhouyuan@gmail.com>
2020-09-03 00:41:12 +0900

    add static tableview

--- FixedUserFontSettingDemo/ViewController.swift
+++ FixedUserFontSettingDemo/ViewController.swift
@@ -12,9 +12,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
+class StaticTableViewController: UITableViewController {
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = super.tableView(tableView, cellForRowAt: indexPath)
+
+        switch indexPath {
+        case [0, 1]: // section:1, row:2, cell for code-setting font
+            cell.textLabel?.text = "Cell-1"
+        case [1, 1]: // section:2, row:2, cell for storyboard-setting font: System
+            break
+        case [2, 1]: // section:3, row:2, cell for storyboard-setting font: Body & Auto-Adjust-Font
+            break
+        case [3, 1]: // section:4, row:2, cell for storyboard-setting font: Body & Non-Auto-Adjust-Font
+            break
+        default: break
+        }
+
+        return cell
+    }
+}
